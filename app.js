5a21b70fa5ed184326b10fe36f3eb05f172d0cda
LeeChanSoo98 <neptune126@naver.com>
2024-08-28 10:31:58 +0900

    다지움

--- app.js
+++ app.js
@@ -2,12 +2,4 @@ console.log("hello");
 console.log(1 + 2);
 console.log(2 + 3);
 console.log("흥선대원군"); //개발자 A가 수정함
-console.log(2 + 3);
-colsole.log(4 + 5);
-console.log("김진사"); //다른개발자가 수정함
-console.log("웹에서 변경");
-console.log("로컬에서 변경");
-console.log("웹에서 로컬로");
-console.log("로컬->웹");
-console.log("dsddsdsdsdsds");
-console.log("찬수");
+console.log("필요없는것은 다지웠다.");
